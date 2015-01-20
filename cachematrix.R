2c1a2520e8820ccecddcb38e12fc1c9697ffe00a
Sauder1973 <wessauder@outlook.com>
2015-01-19 23:42:25 -0500

    Added my Name as Author

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+## Written By Wes Sauder
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
